3f0419393fa3a5a5bcb5d68df2552a3926dafef8
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-01-25 18:25:00 -0800

    finally almost there

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var cookies = {
+    type: salmon,
+    stores: 5,
+    location:{
+        seattle: {
+          name: 'Cookie Store'
+        },
+        stores: [
+          '1st and Pike',
+          'SeaTac Airport',
+          'Seattle Center',
+          'Capitol Hill',
+          'Alki'
+        ]
+      },
