6984cd42eb17c90bc496560b49817f8d5b764278
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-01-31 20:48:58 -0800

    got it

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var stores = [
   new CookieStore('First and Pike', 23, 65, 6.3),
